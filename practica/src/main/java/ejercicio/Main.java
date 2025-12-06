9dd4091dcaa7928982e6319649b6f87fc5700951
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-06 00:43:34 -0500

    fix method menorAMayor
    XD

--- practica/src/main/java/ejercicio/Main.java
+++ practica/src/main/java/ejercicio/Main.java
@@ -57,12 +57,12 @@ public class Main {
     }
 
     public static int[] menorAMayor (int[] arregloN){
-        for(int i = 0; i < arregloN.length - 1; i++){
-            for(int j = 0; j < arregloN.length - 1 - i; i++){
-                if(arregloN[j] > arregloN[i + 1]){
+        for(int i = 0; i < arregloN.length; i++){
+            for(int j = 0; j < arregloN.length; j++){
+                if(arregloN[j] > arregloN[i]){
                     int temp = arregloN[j];
-                    arregloN[j] = arregloN[j + 1];
-                    arregloN[j + 1] = temp;
+                    arregloN[j] = arregloN[i];
+                    arregloN[i] = temp;
                 }
             }
         }

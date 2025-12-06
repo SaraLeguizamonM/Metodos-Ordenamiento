d542917949599e484827c4192f857cd5201b339b
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-05 23:53:53 -0500

    add Methods for point 3
    (In class)

--- practica/src/main/java/ejercicio/Main.java
+++ practica/src/main/java/ejercicio/Main.java
@@ -30,4 +30,44 @@ public class Main {
         }
         return arregloN;
     }
+
+        public static int[] pares(int[] arreglo){
+        int acum = 0;
+        for (int i = 0; i < arreglo.length - 1; i++){
+            if(arreglo[i] % 2 == 0){
+                acum ++;
+            }
+        }
+
+        int[] pares = new int[acum];
+        int par = 0;
+        for (int i = 0; i <arreglo.length - 1; i++){
+            if(arreglo[i] % 2 == 0){
+                pares[par] = arreglo[i];
+                par++;
+            }
+        }
+        pares = menorAMayor(pares);
+        return pares;
+    }
+
+    public static int[] impares(int[] arreglo){
+        int acum = 0;
+        for (int i = 0; i < arreglo.length - 1; i++){
+            if(arreglo[i] % 2 != 0){
+                acum ++;
+            }
+        }
+
+        int[] impares = new int[acum];
+        int impar = 0;
+        for (int i = 0; i <arreglo.length - 1; i++){
+            if(arreglo[i] % 2 != 0){
+                impares[impar] = arreglo[i];
+                impar++;
+            }
+        }
+        impares = menorAMayor(impares);
+        return impares;
+    }
 }

8c0743a7267e4cc2b7cc65b630d760ec71c15e43
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-06 00:27:12 -0500

    create menu

--- practica/src/main/java/ejercicio/Main.java
+++ practica/src/main/java/ejercicio/Main.java
@@ -1,7 +1,58 @@
 package ejercicio;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        String opcion = "";
+
+        System.out.println("Bienvenido a un programa de Metodos de Ordenamiento.");
+        System.out.println("Que tan grande quieres que sea el arreglo?");
+        int[] arreglo = new int[sc.nextInt()];
+        for(int i = 0; i < arreglo.length; i++){
+            System.out.println("Introduce el valor del arreglo " + (i + 1));
+            arreglo[i] = sc.nextInt();
+        }
+        sc.nextLine();
+
+        do{
+
+            System.out.println("Que desea hacer?");
+            System.out.println("1. Ordenamiento de un arreglo de menor a mayor.");
+            System.out.println("2. Ordenamiento de un arreglo de mayor a menor.");
+            System.out.println("3. Ordenamiento de un arreglo (par al inicio e impares al final) menor a mayor");
+            String tipo;
+            do{
+                tipo = sc.nextLine();
+                switch(tipo){
+                    case "1":
+
+                        System.out.println("Tu arreglo ordenado seria... " + (Arrays.toString(menorAMayor(arreglo))));
+                        break;
+                    case "2":
+                        System.out.println("Tu arreglo ordenado seria... " + (Arrays.toString(mayorAMenor(arreglo))));
+                        break;
+                    case "3":
+                        System.out.println("Tu arreglo ordenado seria... " + (Arrays.toString(parEImpar(arreglo))));
+                        break;
+                    default:
+                        System.out.println("Opción inválida, vuelve a intentar.");
+
+                }
+            }while (!tipo.equals("1") && !tipo.equals("2") && !tipo.equals("3"));
+
+            do {
+                System.out.println("¿Quieres hacer otra cosa? (si/no)");
+                opcion = sc.nextLine().toLowerCase();
+
+                if (!opcion.equals("si") && !opcion.equals("no")) {
+                    System.out.println("Opción inválida, por favor coloca una opción válida");
+                }
+
+            } while (!opcion.equals("si") && !opcion.equals("no"));
+
+        }while(opcion.equals("si"));
 
     }
 

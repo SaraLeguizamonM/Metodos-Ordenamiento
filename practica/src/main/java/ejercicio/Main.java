14d51b822e949625df8bbf470dfe9537e2bf44b9
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-06 00:54:11 -0500

    Fix methods (pares, par e impar)
    Only use -1 in cases where it is equal. XD

--- practica/src/main/java/ejercicio/Main.java
+++ practica/src/main/java/ejercicio/Main.java
@@ -80,9 +80,23 @@ public class Main {
         }
         return arregloN;
     }
-        public static int[] pares(int[] arreglo){
+    public static int[] parEImpar(int[] arreglo){
+        int[] arregloOrdenado = new int[arreglo.length];
+        int[] arregloPar = pares(arreglo);
+        int[] arregloImpar = impares(arreglo);
+
+        for (int i = 0; i < arregloPar.length ; i++) {
+            arregloOrdenado[i] = arregloPar[i];
+        }
+        for (int i = 0; i < arregloImpar.length ; i++) {
+            arregloOrdenado[i + arregloPar.length] = arregloImpar[i];
+        }
+        return arregloOrdenado;
+    }
+
+    public static int[] pares(int[] arreglo){
         int acum = 0;
-        for (int i = 0; i < arreglo.length - 1; i++){
+        for (int i = 0; i < arreglo.length ; i++){
             if(arreglo[i] % 2 == 0){
                 acum ++;
             }
@@ -90,7 +104,7 @@ public class Main {
 
         int[] pares = new int[acum];
         int par = 0;
-        for (int i = 0; i <arreglo.length - 1; i++){
+        for (int i = 0; i <arreglo.length ; i++){
             if(arreglo[i] % 2 == 0){
                 pares[par] = arreglo[i];
                 par++;
@@ -99,10 +113,9 @@ public class Main {
         pares = menorAMayor(pares);
         return pares;
     }
-
     public static int[] impares(int[] arreglo){
         int acum = 0;
-        for (int i = 0; i < arreglo.length - 1; i++){
+        for (int i = 0; i < arreglo.length ; i++){
             if(arreglo[i] % 2 != 0){
                 acum ++;
             }
@@ -110,7 +123,7 @@ public class Main {
 
         int[] impares = new int[acum];
         int impar = 0;
-        for (int i = 0; i <arreglo.length - 1; i++){
+        for (int i = 0; i <arreglo.length ; i++){
             if(arreglo[i] % 2 != 0){
                 impares[impar] = arreglo[i];
                 impar++;
@@ -118,19 +131,5 @@ public class Main {
         }
         impares = menorAMayor(impares);
         return impares;
-    }
-
-    public static int[] parEImpar(int[] arreglo){
-        int[] arregloOrdenado = new int[arreglo.length];
-        int[] arregloPar = pares(arreglo);
-        int[] arregloImpar = impares(arreglo);
-
-        for (int i = 0; i < arregloPar.length - 1; i++) {
-            arregloOrdenado[i] = arregloPar[i];
-        }
-        for (int i = 0; i < arregloImpar.length - 1; i++) {
-            arregloOrdenado[i + arregloPar.length] = arregloImpar[i];
-        }
-        return arregloOrdenado;
     }
 }

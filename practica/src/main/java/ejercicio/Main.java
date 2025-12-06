bd2568b1cb141f1dfb18849d9fd0c9bf2b56b06d
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-05 23:11:53 -0500

    Methods menorAMayor and mayorAMenor
    The both methods created in the last class with Bruno

--- practica/src/main/java/ejercicio/Main.java
+++ practica/src/main/java/ejercicio/Main.java
@@ -2,6 +2,32 @@ package ejercicio;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public static int[] menorAMayor (int[] arregloN){
+        for(int i = 0; i < arregloN.length - 1; i++){
+            for(int j = 0; j < arregloN.length - 1 - i; i++){
+                if(arregloN[j] > arregloN[j + 1]){
+                    int temp = arregloN[j];
+                    arregloN[j] = arregloN[j + 1];
+                    arregloN[j + 1] = temp;
+                }
+            }
+        }
+        return arregloN;
+    }
+
+    public static int[] mayorAMenor (int[] arregloN){
+        for(int i = 0; i < arregloN.length - 1; i++){
+            for(int j = 0; j < arregloN.length - 1 - i; i++){
+                if(arregloN[j] < arregloN[j + 1]){
+                    int temp = arregloN[j];
+                    arregloN[j] = arregloN[j + 1];
+                    arregloN[j + 1] = temp;
+                }
+            }
+        }
+        return arregloN;
     }
 }

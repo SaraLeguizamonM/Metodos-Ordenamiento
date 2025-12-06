37398ff1568c12006939bb17b22c134b069e1fe2
Avr1lls <saraleguizamon1234@gmail.com>
2025-12-06 00:30:38 -0500

    fix method mayorAMenor

--- practica/src/main/java/ejercicio/Main.java
+++ practica/src/main/java/ejercicio/Main.java
@@ -72,7 +72,7 @@ public class Main {
     public static int[] mayorAMenor (int[] arregloN){
         for(int i = 0; i < arregloN.length - 1; i++){
             for(int j = 0; j < arregloN.length - 1 - i; i++){
-                if(arregloN[j] < arregloN[j + 1]){
+                if(arregloN[j] < arregloN[i + 1]){
                     int temp = arregloN[j];
                     arregloN[j] = arregloN[j + 1];
                     arregloN[j + 1] = temp;
